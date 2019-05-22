1fd46088fec2e7143f8cbf2f504d96e3e1c867da
Sevann Radhak T <sevann.radhak@gmail.com>
2019-05-22 08:33:53 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// Router
+import Router from './components/Router';
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
